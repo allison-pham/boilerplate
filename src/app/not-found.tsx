775a1eef8785037cca054f59fda202401242664d
allison-pham <allisonppham@gmail.com>
2025-06-21 21:47:18 -0700

    Set up 404 error page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-white dark:bg-black text-black dark:text-white">
+      <h1 className="text-5xl font-bold mb-4">404</h1>
+      <p className="text-lg text-center">Sorry, the page you are looking for does not exist.</p>
+    </div>
+  );
+}
